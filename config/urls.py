ef76139eff0921b222fb039f34a693ee0e282237
= <choiman2021@gmail.com>
2023-04-24 23:13:43 +0900

    login 페이지 생성

--- config/urls.py
+++ config/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from config.views import index # index import
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path( "", index ) # 경로가 없을 때 index View 연결
+    path( "", index ), # 경로가 없을 때 index View 연결
+    path( "users/", include( "users.urls" ) ),
 ]
 
 urlpatterns += static(
