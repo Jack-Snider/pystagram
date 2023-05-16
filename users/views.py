b3ed428b7d58082da853dcf177645d71c82dbccd
= <choiman2021@gmail.com>
2023-05-16 21:22:17 +0900

    로그인,로그아웃 페이지 구현 후 css입힘

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from users.forms import LoginForm
 
@@ -28,9 +28,10 @@ def login_view( request ):
                 login( request, user )
                 return redirect( "/posts/feeds/" )
 
-            # 사용자가 없다면 "실패했습니다" 로그 출력
+            # 사용자가 없다면 form에 에러 추가
             else:
-                print( "로그인에 실패했습니다." )
+                #print( "로그인에 실패했습니다." )
+                form.add_error( None, '입력학 자격증명에 해당하는 사용자가 없습니다.' )
 
         # 어떤 경우든 실패한 경우( 데이터 검증, 사용자 검사 ) 다시 LoginForm을 사용한 로그인 페이지 렌더링
         context = { "form" : form }
@@ -39,3 +40,12 @@ def login_view( request ):
         form = LoginForm()
         context = { "form" : form }
         return render( request, "users/login.html", context )
+
+
+def logout_view( request ):
+
+    # logout 함수 호출에 request를 전달한다.
+    logout( request )
+
+    # logout 처리 후, 로그인 페이지로 이동한다.
+    return redirect( "/users/login/" )
