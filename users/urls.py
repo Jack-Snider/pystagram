9465e175ab3033fa22db7eb9896d26617c9dbdfd
= <choiman2021@gmail.com>
2023-05-18 23:40:54 +0900

    회원가입 페이지 작성

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from users.views import login_view, logout_view
+from users.views import login_view, logout_view, signup
 from posts.views import feeds
 
 urlpatterns = [
     path( "login/", login_view ),
     path( "logout/", logout_view ),
+    path( "signup/", signup ),
 ]
