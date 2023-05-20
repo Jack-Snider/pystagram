39f1c06909a5c9ed94e70adca27c70aed02fbe20
= <choiman2021@gmail.com>
2023-05-21 02:49:40 +0900

    게시물 등록하기( 관리자 화면 )

--- posts/models.py
+++ posts/models.py
@@ -8,9 +8,36 @@ class Post( models.Model ):
     # 같은 앱에서 같은 파일( models.py )에서는 바로 변수를 파라미터로 집어넣는듯.
     user = models.ForeignKey(
         "users.User", # users앱의 User 모델
-        verbose_name = "Author",
+        verbose_name = "작성자",
         on_delete = models.CASCADE,
     )
 
     content = models.TextField( "content" )
-    created = models.DateTimeField( "created at", auto_created = True )
+    created = models.DateTimeField( "created at", auto_now_add = True )
+
+class PostImage( models.Model ):
+
+    post = models.ForeignKey(
+        Post,
+        verbose_name = "포스트",
+        on_delete = models.CASCADE,
+    )
+
+    photo = models.ImageField( "사진", upload_to = "post" )
+
+class Comment( models.Model ):
+
+    user = models.ForeignKey(
+        "users.User",
+        verbose_name = "작성자",
+        on_delete = models.CASCADE,
+    )
+
+    post = models.ForeignKey(
+        Post,
+        verbose_name = "포스트",
+        on_delete = models.CASCADE,
+    )
+
+    content = models.TextField( "내용" )
+    created = models.DateTimeField( "생성일시", auto_now_add = True )
